751abe2571fa2f921a134b3cb85278448ac61de8
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-10-13 12:28:56 +0000

    add

--- __tests__/json/page.test.tsx
+++ __tests__/json/page.test.tsx
@@ -1,29 +1,34 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
-import { setupServer } from 'msw/node';
+import { setupServer } from 'msw/node'; // MSW (Mock Service Worker) によるモックサーバのセットアップ
 import { rest } from 'msw';
 import Page from '../../src/app/json/page';
 
+// テスト用のダミーデータ
 const data = [
   { userId: 1, id: 1, title: "Sample title 1", body: "Sample body 1" },
   { userId: 2, id: 2, title: "Sample title 2", body: "Sample body 2" }
 ];
 
+// MSWを使用してAPIリクエストをモックするハンドラの設定
 const handlers = [
   rest.get('https://jsonplaceholder.typicode.com/posts', (req, res, ctx) => res(ctx.json(data)))
 ];
 
+// モックサーバの初期化
 const server = setupServer(...handlers);
 
 describe('Page', () => {
+  // テストの前にモックサーバを起動し、後処理を行う設定
   beforeAll(() => server.listen());
   afterEach(() => server.resetHandlers());
   afterAll(() => server.close());
 
   it('displays data', async () => {
-    render(<Page />);
+    render(<Page />); // コンポーネントをレンダリング
     for (let item of data) {
-      await waitFor(() => screen.getByText(item.title));
+      await waitFor(() => screen.getByText(item.title)); // タイトルが表示されるまで待機
+      // レンダリングされたコンポーネント内にテキストが存在することを検証
       expect(screen.getByText(item.title)).toBeInTheDocument();
       expect(screen.getByText(item.body)).toBeInTheDocument();
     }
--- __tests__/json/page_without_msw.test.tsx
+++ __tests__/json/page_without_msw.test.tsx
@@ -3,8 +3,10 @@ import { render, screen, waitFor } from '@testing-library/react';
 import Page from '../../src/app/json/page';
 import axios from 'axios';
 
+// axiosのモック化設定
 const mock = axios as jest.Mocked<typeof axios>;
 
+// axiosの動作をモックする設定
 jest.mock('axios');
 
 describe('Page', () => {
@@ -14,15 +16,17 @@ describe('Page', () => {
   ];
 
   it('fetches data', async () => {
+    // API呼び出しの動作をモック化
     mock.get.mockImplementationOnce((url) => {
       if (url === 'https://jsonplaceholder.typicode.com/posts') return Promise.resolve({ data });
       return Promise.reject(new Error('Not found'));
     });
 
-    render(<Page />);
+    render(<Page />); // コンポーネントをレンダリング
     
     for (let item of data) {
-      await waitFor(() => screen.getByText(item.title));
+      await waitFor(() => screen.getByText(item.title)); // タイトルが表示されるまで待機
+      // レンダリングされたコンポーネント内にテキストが存在することを検証
       expect(screen.getByText(item.title)).toBeInTheDocument();
       expect(screen.getByText(item.body)).toBeInTheDocument();
     }
