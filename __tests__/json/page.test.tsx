7716ca98e2a3efd67c44c31046173540a9f4dfa5
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-10-13 12:17:13 +0000

    add

--- __tests__/json/page.test.tsx
+++ __tests__/json/page.test.tsx
@@ -1,27 +1,20 @@
+// __tests__/json/page.test.tsx
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import { setupServer } from 'msw/node';
 import { rest } from 'msw';
 import PostsPage from '../../src/app/json/page';
 
+const posts = [
+  { userId: 1, id: 1, title: "Sample title 1", body: "Sample body 1" },
+  { userId: 2, id: 2, title: "Sample title 2", body: "Sample body 2" }
+];
+
 // Define mock server handlers
 const postHandlers = [
   rest.get('https://jsonplaceholder.typicode.com/posts', (req, res, ctx) => {
     return res(
-      ctx.json([
-        {
-          userId: 1,
-          id: 1,
-          title: "Sample title 1",
-          body: "Sample body 1"
-        },
-        {
-          userId: 2,
-          id: 2,
-          title: "Sample title 2",
-          body: "Sample body 2"
-        },
-      ])
+      ctx.json(posts)
     );
   })
 ];
@@ -39,11 +32,10 @@ describe('PostsPage', () => {
     render(<PostsPage />);
     
     // Wait for the mocked API call to resolve and render
-    await waitFor(() => screen.getByText("Sample title 1"));
-
-    expect(screen.getByText("Sample title 1")).toBeInTheDocument();
-    expect(screen.getByText("Sample body 1")).toBeInTheDocument();
-    expect(screen.getByText("Sample title 2")).toBeInTheDocument();
-    expect(screen.getByText("Sample body 2")).toBeInTheDocument();
+    for (let post of posts) {
+      await waitFor(() => screen.getByText(post.title));
+      expect(screen.getByText(post.title)).toBeInTheDocument();
+      expect(screen.getByText(post.body)).toBeInTheDocument();
+    }
   });
 });
--- __tests__/json/page_without_msw.test.tsx
+++ __tests__/json/page_without_msw.test.tsx
@@ -1,3 +1,4 @@
+// __tests__/json/page_without_msw.test.tsx
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import PostsPage from '../../src/app/json/page';
@@ -9,25 +10,15 @@ const axiosMock = axios as jest.Mocked<typeof axios>;
 jest.mock('axios');
 
 describe('PostsPage', () => {
-  const mockPosts = [
-    {
-      userId: 1,
-      id: 1,
-      title: "Sample title 1",
-      body: "Sample body 1"
-    },
-    {
-      userId: 2,
-      id: 2,
-      title: "Sample title 2",
-      body: "Sample body 2"
-    }
+  const posts = [
+    { userId: 1, id: 1, title: "Sample title 1", body: "Sample body 1" },
+    { userId: 2, id: 2, title: "Sample title 2", body: "Sample body 2" }
   ];
 
   it('fetches and displays posts', async () => {
     axiosMock.get.mockImplementationOnce((url) => {
       if (url === 'https://jsonplaceholder.typicode.com/posts') {
-        return Promise.resolve({ data: mockPosts });
+        return Promise.resolve({ data: posts });
       }
       return Promise.reject(new Error('Not found'));
     });
@@ -35,7 +26,7 @@ describe('PostsPage', () => {
     render(<PostsPage />);
     
     // Wait until the posts are displayed
-    for (let post of mockPosts) {
+    for (let post of posts) {
       await waitFor(() => screen.getByText(post.title));
       expect(screen.getByText(post.title)).toBeInTheDocument();
       expect(screen.getByText(post.body)).toBeInTheDocument();
