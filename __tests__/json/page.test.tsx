386ee28ea18100b0d4960966dba275e880210447
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-10-13 12:24:12 +0000

    add

--- __tests__/json/page.test.tsx
+++ __tests__/json/page.test.tsx
@@ -1,41 +1,31 @@
-// __tests__/json/page.test.tsx
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import { setupServer } from 'msw/node';
 import { rest } from 'msw';
-import PostsPage from '../../src/app/json/page';
+import Page from '../../src/app/json/page';
 
-const posts = [
+const data = [
   { userId: 1, id: 1, title: "Sample title 1", body: "Sample body 1" },
   { userId: 2, id: 2, title: "Sample title 2", body: "Sample body 2" }
 ];
 
-// Define mock server handlers
-const postHandlers = [
-  rest.get('https://jsonplaceholder.typicode.com/posts', (req, res, ctx) => {
-    return res(
-      ctx.json(posts)
-    );
-  })
+const handlers = [
+  rest.get('https://jsonplaceholder.typicode.com/posts', (req, res, ctx) => res(ctx.json(data)))
 ];
 
-// Initialize mock server
-const mockServer = setupServer(...postHandlers);
+const server = setupServer(...handlers);
 
-describe('PostsPage', () => {
-  // Setup and cleanup for Jest tests with mock server
-  beforeAll(() => mockServer.listen());
-  afterEach(() => mockServer.resetHandlers());
-  afterAll(() => mockServer.close());
+describe('Page', () => {
+  beforeAll(() => server.listen());
+  afterEach(() => server.resetHandlers());
+  afterAll(() => server.close());
 
-  it('should display fetched posts', async () => {
-    render(<PostsPage />);
-    
-    // Wait for the mocked API call to resolve and render
-    for (let post of posts) {
-      await waitFor(() => screen.getByText(post.title));
-      expect(screen.getByText(post.title)).toBeInTheDocument();
-      expect(screen.getByText(post.body)).toBeInTheDocument();
+  it('displays data', async () => {
+    render(<Page />);
+    for (let item of data) {
+      await waitFor(() => screen.getByText(item.title));
+      expect(screen.getByText(item.title)).toBeInTheDocument();
+      expect(screen.getByText(item.body)).toBeInTheDocument();
     }
   });
 });
--- __tests__/json/page_without_msw.test.tsx
+++ __tests__/json/page_without_msw.test.tsx
@@ -1,35 +1,30 @@
-// __tests__/json/page_without_msw.test.tsx
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
-import PostsPage from '../../src/app/json/page';
+import Page from '../../src/app/json/page';
 import axios from 'axios';
 
-// Setup Axios mock
-const axiosMock = axios as jest.Mocked<typeof axios>;
+const mock = axios as jest.Mocked<typeof axios>;
 
 jest.mock('axios');
 
-describe('PostsPage', () => {
-  const posts = [
+describe('Page', () => {
+  const data = [
     { userId: 1, id: 1, title: "Sample title 1", body: "Sample body 1" },
     { userId: 2, id: 2, title: "Sample title 2", body: "Sample body 2" }
   ];
 
-  it('fetches and displays posts', async () => {
-    axiosMock.get.mockImplementationOnce((url) => {
-      if (url === 'https://jsonplaceholder.typicode.com/posts') {
-        return Promise.resolve({ data: posts });
-      }
+  it('fetches data', async () => {
+    mock.get.mockImplementationOnce((url) => {
+      if (url === 'https://jsonplaceholder.typicode.com/posts') return Promise.resolve({ data });
       return Promise.reject(new Error('Not found'));
     });
 
-    render(<PostsPage />);
+    render(<Page />);
     
-    // Wait until the posts are displayed
-    for (let post of posts) {
-      await waitFor(() => screen.getByText(post.title));
-      expect(screen.getByText(post.title)).toBeInTheDocument();
-      expect(screen.getByText(post.body)).toBeInTheDocument();
+    for (let item of data) {
+      await waitFor(() => screen.getByText(item.title));
+      expect(screen.getByText(item.title)).toBeInTheDocument();
+      expect(screen.getByText(item.body)).toBeInTheDocument();
     }
   });
 });
